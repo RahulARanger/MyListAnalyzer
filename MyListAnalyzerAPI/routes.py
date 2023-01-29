ff579d94bba2cb0e6234a23f4872232462395898
Rahul <saihanumarahul66@gmail.com>
2023-01-29 22:33:08 +0530

    -- Added a Playground branch in MyAnlistListAnalyzer - API for best pratice
    -- Refer playground branch only to make changes in the MyListAnalyzerDash

--- MyListAnalyzerAPI/routes.py
+++ MyListAnalyzerAPI/routes.py
@@ -1,53 +1,23 @@
-import logging
-
+import json
+import pathlib
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Mount, Route
-
-from MyListAnalyzerAPI.fetch_sources import MALSession
-from MyListAnalyzerAPI.modals import ForUserAnimeList
 from MyListAnalyzerAPI.parse_user_anime_list import give_over_view, generate_report_for_recent_animes, \
     parse_user_anime_list, fetch_recent_animes
 
 
-async def validate_user(request: Request):
-    user_name = "@me"
-    try:
-        user_name = request.query_params.get("user_name", "")
-        token = request.headers.get("token", "")
-        user_name, reason = await MALSession().who_is_the_user(user_name, token)
-    except Exception as error:
-        reason = repr(error)
+def from_json_to_json_resp(test_data):
+    with (pathlib.Path(__file__).parent / "Test Data" / test_data).open() as read:
+        return JSONResponse(content=json.load(read), status_code=200)
 
-    failed = bool(reason)
-    return JSONResponse(
-        content=dict(passed=not failed, reason=reason, user_name=user_name),
-        status_code=406 if failed else 200
-    )
+
+async def validate_user(request: Request):
+    return from_json_to_json_resp("validateUser.psd")
 
 
 async def fetch_user_anime_list(request: Request):
-    token = request.headers.get("token", "")
-    user_name = "@me"
-    reason = False
-    is_nsfw = request.headers.get("nsfw", False)
-    user_content = dict(user_name=user_name)
-
-    try:
-        temp = ForUserAnimeList(**request.query_params)
-        user_name = temp.user_name if temp.user_name else user_name
-        user_content.update(user_name=user_name)
-        user_content.update(await MALSession().fetch_list(user_name, token, temp.url, nsfw=is_nsfw))
-    except Exception as error:
-        reason = repr(error)
-        logging.exception("Failed to fetch the User Anime List", exc_info=True)
-
-    failed = bool(reason)
-
-    return JSONResponse(
-        content=dict(passed=not failed, reason=reason, **user_content),
-        status_code=406 if failed else 200
-    )
+    return from_json_to_json_resp("fetchUserAnimeList.psd")
 
 
 dynamic_table_routes = Mount(path="/dynamic", routes=[
